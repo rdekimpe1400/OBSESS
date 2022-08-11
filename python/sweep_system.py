038e334bfab4fd8be75d98a1c17794cb7e7fd026
Remi Dekimpe <remi.dekimpe@uclouvain.be>
2022-08-11 17:17:49 +0200

    modify scripts

--- python/sweep_system.py
+++ python/sweep_system.py
@@ -6,28 +6,29 @@ from src import default
 
 if __name__ == "__main__":
   
-  scale_signal = 0.1
-  
-  opti.out_dir = './temp/sweep_all/'
-  opti.clear_output_dir(opti.out_dir)
+  for i in range(2,20):
+    scale_signal = 0.1
+    
+    opti.out_dir = './temp/sweep_all_{:d}/'.format(i)
+    opti.clear_output_dir(opti.out_dir)
 
-  opti.params = default.default_parameters()
-  opti.params['input_scale'] = scale_signal
-  
-  ref = [0.5,0.5,0.6,1.0,1.0]
-  opti.execute_framework([0,ref[1],ref[2],ref[3],ref[4]])
-  opti.execute_framework([ref[0],0,ref[2],ref[3],ref[4]])
-  opti.execute_framework([ref[0],ref[1],0,ref[3],ref[4]])
-  opti.execute_framework([ref[0],ref[1],ref[2],0,ref[4]])
-  opti.execute_framework([ref[0],ref[1],ref[2],ref[3],0])
-  
-  scale_max=6
-  for scale in range(0,scale_max+1):
-    n_steps = np.power(2,scale)
-    for i in range(0,n_steps+1):
-      if (np.mod(i,2)>0):
-        opti.execute_framework([i/n_steps,ref[1],ref[2],ref[3],ref[4]])
-        opti.execute_framework([ref[0],i/n_steps,ref[2],ref[3],ref[4]])
-        opti.execute_framework([ref[0],ref[1],i/n_steps,ref[3],ref[4]])
-        opti.execute_framework([ref[0],ref[1],ref[2],i/n_steps,ref[4]])
-        opti.execute_framework([ref[0],ref[1],ref[2],ref[3],i/n_steps])
+    opti.params = default.default_parameters()
+    opti.params['input_scale'] = scale_signal
+    
+    ref = [0.5,0.5,0.6,1.0,1.0]
+    opti.execute_framework([0,ref[1],ref[2],ref[3],ref[4]])
+    opti.execute_framework([ref[0],0,ref[2],ref[3],ref[4]])
+    opti.execute_framework([ref[0],ref[1],0,ref[3],ref[4]])
+    opti.execute_framework([ref[0],ref[1],ref[2],0,ref[4]])
+    opti.execute_framework([ref[0],ref[1],ref[2],ref[3],0])
+    
+    scale_max=4
+    for scale in range(0,scale_max+1):
+      n_steps = np.power(2,scale)
+      for i in range(0,n_steps+1):
+        if (np.mod(i,2)>0):
+          opti.execute_framework([i/n_steps,ref[1],ref[2],ref[3],ref[4]])
+          opti.execute_framework([ref[0],i/n_steps,ref[2],ref[3],ref[4]])
+          opti.execute_framework([ref[0],ref[1],i/n_steps,ref[3],ref[4]])
+          opti.execute_framework([ref[0],ref[1],ref[2],i/n_steps,ref[4]])
+          opti.execute_framework([ref[0],ref[1],ref[2],ref[3],i/n_steps])
