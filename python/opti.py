fix param computation

python/opti.py
@@ -62,6 +62,11 @@ def norm_param(x):
   noise_IA = np.power(10.0,np.log10(IA_noise_range[1])-x[0]*(np.log10(IA_noise_range[1])-np.log10(IA_noise_range[0])))
   params["IA_thermal_noise"] = noise_IA/10
   
+  #FREQ
+  freq = FREQ_range[0] + x[3]*(FREQ_range[1]-FREQ_range[0])
+  params['ADC_Fs'] = freq
+  changeFrequency(freq)
+  
   #ADC
   res_ADC = ADC_resolution_range[0] + x[1]*(ADC_resolution_range[1]-ADC_resolution_range[0])
   VCO_freq = np.power(2.0,res_ADC)*params['ADC_Fs']
@@ -70,11 +75,6 @@ def norm_param(x):
   #SVM
   SVM_pruning = SVM_pruning_range[1] - x[2]*(SVM_pruning_range[1]-SVM_pruning_range[0])
   params['SVM_pruning_D'] = SVM_pruning
-  
-  #FREQ
-  freq = FREQ_range[0] + x[3]*(FREQ_range[1]-FREQ_range[0])
-  params['ADC_Fs'] = freq
-  changeFrequency(freq)
   
   #FEAT
   feat = FEAT_range[0] + x[4]*(FEAT_range[1]-FEAT_range[0])

python/sweep_system.py
@@ -6,23 +6,27 @@ from src import default
 
 if __name__ == "__main__":
   
-  for i in range(2,20):
+  for j in range(0,20):
     scale_signal = 0.1
     
-    opti.out_dir = './temp/sweep_all_{:d}/'.format(i)
+    opti.out_dir = './temp/sweep_syst_{:d}/'.format(j)
     opti.clear_output_dir(opti.out_dir)
 
     opti.params = default.default_parameters()
     opti.params['input_scale'] = scale_signal
     
+    opti.execute_framework([1.0,1.0,0.6,1.0,1.0])
+    
     ref = [0.5,0.5,0.6,1.0,1.0]
+    opti.execute_framework([ref[0],ref[1],ref[2],ref[3],ref[4]])
+    
     opti.execute_framework([0,ref[1],ref[2],ref[3],ref[4]])
     opti.execute_framework([ref[0],0,ref[2],ref[3],ref[4]])
     opti.execute_framework([ref[0],ref[1],0,ref[3],ref[4]])
     opti.execute_framework([ref[0],ref[1],ref[2],0,ref[4]])
     opti.execute_framework([ref[0],ref[1],ref[2],ref[3],0])
     
-    scale_max=4
+    scale_max=5
     for scale in range(0,scale_max+1):
       n_steps = np.power(2,scale)
       for i in range(0,n_steps+1):
@@ -32,3 +36,6 @@ if __name__ == "__main__":
           opti.execute_framework([ref[0],ref[1],i/n_steps,ref[3],ref[4]])
           opti.execute_framework([ref[0],ref[1],ref[2],i/n_steps,ref[4]])
           opti.execute_framework([ref[0],ref[1],ref[2],ref[3],i/n_steps])
+    
+    for i in range(0,50):
+      opti.execute_framework([ref[0],ref[1],ref[2],ref[3],ref[4]])