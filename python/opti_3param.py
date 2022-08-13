0421bab23ed13708517293840eed1b65a4b2b316
Remi Dekimpe <remi.dekimpe@uclouvain.be>
2022-08-13 12:31:15 +0200

    modif script

--- python/opti_3param.py
+++ python/opti_3param.py
@@ -3,13 +3,12 @@ import os
 import opti
 import numpy as np
 from src import default
-from joblib import dump, load
 
 if __name__ == "__main__":
-  dir_name = './temp/opti_grad_{}/'
-  scale = 0.1
   for i in range(0,20):
-    opti.out_dir = dir_name.format(i)
+    dir_name = './temp/opti_3param_{:d}/'.format(i)
+    scale = 0.1
+    opti.out_dir = dir_name
     opti.clear_output_dir(opti.out_dir)
     opti.params = default.default_parameters()
     opti.params['input_scale'] = scale
@@ -17,8 +16,10 @@ if __name__ == "__main__":
     opti.params['input_scale'] = scale
     result = opti.run_optimization(opti.out_dir)
     print(result)
-    f = open('{}result.log'.format(dir_name.format(i)), "w")
+    f = open('{}result.log'.format(dir_name), "w")
     f.write("{} ".format(str(result)))
     f.close()
+    
+  
   
   
